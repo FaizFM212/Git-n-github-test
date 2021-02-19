01d5577e5b800c2df901b081e07eefac7b575ff3
FazeFM212 <faizfm212@gmail.com>
2021-02-19 16:18:53 +0700

    Added calculator

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+userInput = ""
+firstNum  = 0
+operation = "+"
+secondNum = 0
+
+print("Calculator Program")
+print("Type \"quit\" to quit program")
+
+if operation == "+" :
+    print("{0} {1} {2} = {3}".format(firstNum, operation, secondNum, (firstNum + secondNum)))
