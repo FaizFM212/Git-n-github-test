d33ac3ec20bdd35465c92e55602dd826a3e30cf2
FazeFM212 <faizfm212@gmail.com>
2021-02-19 14:55:58 +0700

    Initial commit

---
+++ Repeater.py
@@ -0,0 +1,9 @@
+userInput = ""
+
+print("Repeater Program")
+print("Type \"quit\" to quit program")
+
+while not userInput == "quit" :
+    userInput = input(">")
+    if not userInput == "quit" : 
+        print(userInput)
